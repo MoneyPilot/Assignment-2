cf74de928d9a265ad4805926e09c8b4944ffb081
Danil <94500841+MoneyPilot@users.noreply.github.com>
2022-01-06 09:49:02 -0500

    Add files via upload

---
+++ Map.js
@@ -0,0 +1,10 @@
+function Map(){
+    let myArr = [0,1,'a',2,3,4,5,'b']; //intializing my array
+
+    for(index=0;index<myArr.length;index++){
+        myArr[index]=myArr[index]+2
+    }
+    console.log(myArr);
+
+
+}
---
+++ forEach.js
@@ -0,0 +1,11 @@
+function myEach(){
+
+    let myArr = [0,1,2,3,4,5,'a','b']; //intializing my array
+
+    console.log("Calling myEach() now! "); //msg
+
+    for(int index=0;index<myArr.lengthnewFunction();index++){ //runninf for loop to print out my array in order
+        console.log(myArr[index])+index;
+    }
+
+}
